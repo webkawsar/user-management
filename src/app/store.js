265cadd592be5bb16467cd0fa36c8267dd3a787b
Kawsar <web.kawsarahmed@gmail.com>
2023-06-13 21:21:29 +0600

    remove unnecessary code

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
 import authSliceReducer from "../features/auth/authSlice";
-import profileSliceReducer from "../features/profiles/profileSlice";
 
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authSliceReducer,
-        profile: profileSliceReducer
     },
     middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(apiSlice.middleware)
 })
--- src/features/users/usersAPI.js
+++ src/features/users/usersAPI.js
@@ -3,113 +3,6 @@ import { apiSlice } from "../api/apiSlice";
 export const contactsAPI = apiSlice.injectEndpoints({
   endpoints: (builder) => {
     return {
-      addContact: builder.mutation({
-        query: (contact) => {
-          const { image, ...restData } = contact;
-          const formData = new FormData();
-          formData.append("files.image", image[0], image[0]?.name);
-          formData.append("data", JSON.stringify(restData));
-
-          return {
-            url: `/contacts`,
-            method: "POST",
-            body: formData,
-          };
-        },
-        async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-          try {
-            const result = await queryFulfilled;
-            dispatch(
-              apiSlice.util.updateQueryData(
-                "getContacts",
-                undefined,
-                (draftContacts) => {
-                  draftContacts?.data.unshift(result?.data?.data);
-                }
-              )
-            );
-          } catch (error) {}
-        },
-      }),
-      getContact: builder.query({
-        query: (contactId) => `/contacts/${contactId}?populate=*`,
-      }),
-      updateContact: builder.mutation({
-        query: (data) => {
-          const { image, contactId, ...restData } = data;
-          const formData = new FormData();
-          if (image.length) {
-            formData.append("files.image", image[0], image[0]?.name);
-          }
-          formData.append("data", JSON.stringify(restData));
-
-          return {
-            url: `/contacts/${contactId}`,
-            method: "PUT",
-            body: formData,
-          };
-        },
-        async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-          try {
-            const result = await queryFulfilled;
-
-            // update /contacts/:id data
-            dispatch(
-              apiSlice.util.updateQueryData(
-                "getContact",
-                arg?.contactId.toString(),
-                (draftContact) => result?.data
-              )
-            );
-
-            // update /contacts data
-            dispatch(
-              apiSlice.util.updateQueryData(
-                "getContacts",
-                undefined,
-                (draftContacts) => {
-                  const foundContact = draftContacts?.data.find(
-                    (contact) => contact.id == result?.data?.data?.id
-                  );
-                  foundContact.attributes = result?.data?.data?.attributes;
-                }
-              )
-            );
-          } catch (error) {}
-        },
-      }),
-      deleteContact: builder.mutation({
-        query: (contactId) => ({
-          url: `/contacts/${contactId}`,
-          method: "DELETE",
-        }),
-        async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-          try {
-            const result = await queryFulfilled;
-
-            // update /contacts data
-            dispatch(
-              apiSlice.util.updateQueryData(
-                "getContacts",
-                undefined,
-                (draftContacts) => {
-
-                  // console.log(JSON.parse(JSON.stringify(draftContacts)), 'draftContacts')
-
-                  const filteredContacts = draftContacts?.data.filter(
-                    (contact) => contact.id != result?.data?.data?.id
-                  );
-                  
-                  draftContacts.data = filteredContacts;
-                  draftContacts.meta.pagination.total = draftContacts.meta.pagination.total - 1;
-                  
-                  // console.log(JSON.parse(JSON.stringify(draftContacts)), 'draftContacts')
-                }
-              )
-            );
-          } catch (error) {}
-        },
-      }),
       addUser: builder.mutation({
         query: (data) => ({
           url: `/api/v1/users`,
@@ -181,10 +74,6 @@ export const contactsAPI = apiSlice.injectEndpoints({
 });
 
 export const {
-  useAddContactMutation,
-  useGetContactQuery,
-  useUpdateContactMutation,
-  useDeleteContactMutation,
   useAddUserMutation,
   useGetUsersQuery,
   useGetUserQuery,
